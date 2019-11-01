019233a514b5affb4c12c236842f8ac8c8b90f47
yomZsamora <samora.y@adzumi.co.ke>
2019-11-01 12:47:04 +0300

    Retrieving date submitted to the scripts.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,12 @@ $(document).ready(function(){
         $('[data-toggle="tooltip"]').tooltip();
     });
             
-    $("#date").mask("99/99/9999");
+    $("#myBirthDate").mask("99/99/9999");
 
 });
 
 
+function getAkanName(){
+    var myBirthday = document.getElementById("myBirthDate").value;
+    console.log(myBirthday);
+}
