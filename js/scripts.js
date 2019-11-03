85435d38c9babc35b3fd56d913baea9b81c983ba
yomZsamora <samora.y@adzumi.co.ke>
2019-11-03 11:33:37 +0300

    Configuration : Valifation for the Gender Type Complete.

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,14 @@ $(document).ready(function(){
 
 function getAkanName(){
     var myBirthday = document.getElementById("myBirthDate").value;
+    var myGender = document.getElementsByName("gender");
     var dateOfBirth = new Date(myBirthday);
     var dayOfTheWeek = dateOfBirth.getDay();
     if(myBirthday === ""){
         document.getElementById('message').innerHTML = "<div class=\"alert alert-danger\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">x</button><strong>Oh snap!</strong> You Didn't Submit a Valid Date! <a href=\"\" class=\"alert-link\">Try Again</a>.</div>";
     }
-    else {
-        var gender = document.getElementsByName("gender");
+    else if(!document.getElementsByName('gender').checked) {
+        document.getElementById('message').innerHTML = "<div class=\"alert alert-danger\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">x</button><strong>Oh snap!</strong> You Should Select a Gender Too Determine Your Akan Name! <a href=\"\" class=\"alert-link\">Try Again</a>.</div>";
     }
 
 }
