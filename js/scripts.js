bcef857a4f3e3faa4be8aaf5a482af479bb026f5
yomZsamora <samora.y@adzumi.co.ke>
2019-11-03 11:17:06 +0300

    Working on form validation.

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,10 @@ function getAkanName(){
     var dateOfBirth = new Date(myBirthday);
     var dayOfTheWeek = dateOfBirth.getDay();
     if(myBirthday === ""){
-        document.getElementById('message').innerHTML = "Please Enter a <span>Valid Date</span>.";
+        document.getElementById('message').innerHTML = "<div class=\"alert alert-danger\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">x</button><strong>Oh snap!</strong> You Didn't Submit a Valid Date! <a href=\"\" class=\"alert-link\">Try Again</a>.</div>";
+    }
+    else {
+        var gender = document.getElementsByName("gender");
     }
 
 }
