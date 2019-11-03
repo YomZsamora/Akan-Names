d727dbd1ad5d54a9c5f9fe852d309402b2bf219a
yomZsamora <samora.y@adzumi.co.ke>
2019-11-03 20:45:40 +0300

    Adding styling using JavaScript.

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 
 
 function getAkanName(){
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
     var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
     var myBirthday = document.getElementById("myBirthDate").value;
@@ -19,30 +20,18 @@ function getAkanName(){
         for(var i=0;i<myGender.length;i++){
             if(myGender[i].checked){
                 if(myGender[i].value === "Male"){
+                    document.getElementById('message').innerHTML = "Born on a <span>" + days[dayOfTheWeek] + "</span>, Your Akan Name's <span>" + maleAkanNames[dayOfTheWeek] + "</span>";
                     console.log(maleAkanNames[dayOfTheWeek]);
                 }
                 else {
+                    document.getElementById('message').innerHTML = "Born on a <span>" + days[dayOfTheWeek] + "</span>, Your Akan Name's <span>" + femaleAkanNames[dayOfTheWeek] + "</span>";
                     console.log(femaleAkanNames[dayOfTheWeek]);
                 }
+                break;
+            }    
+            else {
+                document.getElementById('message').innerHTML = "<div class=\"alert alert-danger\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">x</button><strong>Oh snap!</strong> You Should Select a Gender Too Determine Your Akan Name! <a href=\"\" class=\"alert-link\">Try Again</a>.</div>";
             }
         }
-        
     }
-
-
-
-
-    // else if(!document.getElementsByName('gender').checked  ) {
-    //     document.getElementById('message').innerHTML = "<div class=\"alert alert-danger\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">x</button><strong>Oh snap!</strong> You Should Select a Gender Too Determine Your Akan Name! <a href=\"\" class=\"alert-link\">Try Again</a>.</div>";
-    // }
-    // else if(document.getElementsByName('genderMale').checked && !document.getElementsByName('genderFemale').checked) {
-    //     document.getElementById('message').innerHTML = "Born on a <span>" + dayOfTheWeek + "</span>, Your Akan Name's" + maleAkanNames[dayOfTheWeek] + "";
-    // }
-    // else if(document.getElementsByName('genderFemale').checked && !document.getElementsByName('genderMale').checked) {
-    //     document.getElementById('message').innerHTML = "Born on a <span>" + dayOfTheWeek + "</span>, Your Akan Name's" + femaleAkanNames[dayOfTheWeek] + "";
-    // }
-    // else {
-    //         // alert(femaleNames[day]);
-    // }
-
 }
